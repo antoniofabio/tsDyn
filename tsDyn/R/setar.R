acd482012499163988b65f5a66d56e59044c244f
Matthieu Stigler <mat@cunix.(none)>
2009-07-29 23:02:21 +0200

    Fix bug: remove ^in grep
    Grep to get the coef had ^phi and was not working when coef are in diff and marked Dphi, just removed the ^hoping nothing else breaks

--- tsDyn/R/setar.R
+++ tsDyn/R/setar.R
@@ -358,7 +358,7 @@ if(type=="level"){
 getSetarXRegimeCoefs <- function(x, regime=c("L","M","H")) {
 	regime <- match.arg(regime)
 	x <- x$coef
-	x1 <- x[grep(paste("^phi", regime, "\\.", sep=""), names(x))]
+	x1 <- x[grep(paste("phi", regime, "\\.", sep=""), names(x))]
 	x2 <- x[grep(paste("^const ", regime, "$", sep=""), names(x))]
 	x3 <- x[grep(paste("^trend ", regime, "$", sep=""), names(x))]
 	  return(c(x1, x2, x3))
