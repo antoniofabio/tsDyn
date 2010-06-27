ffe0b7118d7e6f416720dc7c67e72b966044c53b
Matthieu Stigler <Matthieu.Stigler@gmail.com>
2010-06-27 13:53:01 +0200

    test of VECM: add few tests

--- tsDyn/tests/VECM.R
+++ tsDyn/tests/VECM.R
@@ -7,15 +7,25 @@ data(zeroyld)
 ## Test a few VECM models
 myVECM1<-VECM(zeroyld, lag=1)
 myVECM2<-VECM(zeroyld, lag=3, include="const")
+myVECM2a<-VECM(zeroyld, lag=3, include="const", beta=-1)
 myVECM3<-VECM(zeroyld, lag=1, estim="ML")
 myVECM4<-VECM(zeroyld, lag=3, estim="ML")
-myVECM4<-VECM(zeroyld, lag=3, estim="ML")
+
 
 summary(myVECM1)
 summary(myVECM2)
+summary(myVECM2a)
 summary(myVECM3)
 summary(myVECM4)
 
+myVECM1$model.specific$coint
+myVECM1$model.specific$beta
+
+myVECM2a$model.specific$coint
+myVECM2a$model.specific$beta
+
+myVECM3$model.specific$coint
+myVECM3$model.specific$beta
 
 
 ###Check Johansen MLE: comparing with vars package
