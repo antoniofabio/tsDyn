73e59c6658cfc415c696ebc29249f6c81a573c02
Matthieu Stigler <Matthieu.Stigler@gmail.com>
2011-05-08 13:09:06 +0200

    test lstar: update, adding deviance

--- tsDyn/tests/lstar.R
+++ tsDyn/tests/lstar.R
@@ -2,6 +2,8 @@ library(tsDyn)
 
 mod.lstar <- lstar(log10(lynx), m=2, mTh=c(0,1), control=list(maxit=3000))
 mod.lstar
+deviance(mod.lstar)
 
 mod.lstar2 <- lstar(log10(lynx), m=1, control=list(maxit=3000))
 mod.lstar2
+deviance(mod.lstar2)
