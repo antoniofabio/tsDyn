1fba71ba1c7425c5f6d1e1fdfaa89a429e8db586
Matthieu Stigler <Matthieu.Stigler@gmail.com>
2011-05-08 13:24:58 +0200

    test lstar: add more test, with summary, AIC...

--- tsDyn/tests/lstar.R
+++ tsDyn/tests/lstar.R
@@ -2,8 +2,12 @@ library(tsDyn)
 
 mod.lstar <- lstar(log10(lynx), m=2, mTh=c(0,1), control=list(maxit=3000))
 mod.lstar
+summary(mod.lstar)
 deviance(mod.lstar)
+c(AIC(mod.lstar),BIC(mod.lstar))
 
 mod.lstar2 <- lstar(log10(lynx), m=1, control=list(maxit=3000))
 mod.lstar2
+summary(mod.lstar2)
 deviance(mod.lstar2)
+c(AIC(mod.lstar2),BIC(mod.lstar2))
